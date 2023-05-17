caeab7e052b68a87bfce4b04b904c5a31b425a2f
kauanNas <101115741+kauanNas@users.noreply.github.com>
2023-05-17 20:14:19 -0300

    layouts prontos

---
+++ Kauan_Nascimento_RM87318/app/src/main/java/br/com/fiap/movies/Tela1Activity.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela1)
+    }
+}
